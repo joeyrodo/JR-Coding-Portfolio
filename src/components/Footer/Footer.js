40b44a8d55baf44f2415a02bbe30eed4926377a0
joeyrodo <joeyrodo@gmail.com>
2025-03-13 15:15:18 -0400

    quick fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faGithub,
     faLinkedin,
-    faEnvelope
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
